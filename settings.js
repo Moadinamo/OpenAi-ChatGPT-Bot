a454a8f8d87d02e699af8391e06338e24e54a391
Moadinamo <145803844+Moadinamo@users.noreply.github.com>
2023-10-04 02:17:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,19 +16,19 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
+global.OWNER_NAME = 'Moad' // Owner Name
+global.OWNER_NUMBER = ['212644072336'] //Owner Number
+global.BOT_ADMINS = ['212664408359','212644072336'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
-global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
-global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
+global.IMAGE_QUALITY = 'high' // Available Quality Are => low | mediam | high
+global.CAPTION = '*©*'
 
 /*
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.AI_PASSWORD = 'Kibg-nima-gpt-bot'
+global.SESSION_ID = 'elisa_md@;;;xVcDhbyb#DtZzcWe2mrUWCpVl3ptX8d4Jm3jfNPu5l39aYPzIlFw'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
@@ -43,8 +43,8 @@ mess = {
 	need_text: '*Please Give Some words*'
 }
 react = {
-    ai_message : '🤖',
-    ai_image : '🤖'
+    ai_message : '♥️',
+    ai_image : '♥️'
 }
 
 
